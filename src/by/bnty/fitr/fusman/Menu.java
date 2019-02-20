4430ae9b012845cd1a79248e610186d467836c55
Admin <maximopolnate@gmail.com>
2019-02-20 23:38:26 +0300

    test commit

--- src/by/bnty/fitr/fusman/Menu.java
+++ src/by/bnty/fitr/fusman/Menu.java
@@ -1,7 +1,7 @@
 package by.bnty.fitr.fusman;
 
 public class Menu {
-    public static void main(String args[]) {
-
+    public static void main(String[] args) {
+        System.out.println("hello world");
     }
 }
