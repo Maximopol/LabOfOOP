21380d3f599ab87fa6789036d9941b407ccbcd28
Admin <maximopolnate@gmail.com>
2019-02-21 11:18:13 +0300

    test commit

---
+++ src/by/bnty/fitr/fusman/division/Division.java
@@ -0,0 +1,12 @@
+package by.bnty.fitr.fusman.division;
+
+import com.sun.org.apache.xpath.internal.operations.Div;
+
+public class Division {
+    public Division(){
+
+    }
+    public Division(Division division){
+
+    }
+}
