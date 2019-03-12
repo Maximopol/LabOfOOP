edd6411255235f09007ad3ee6970f6890f6c84f9
Admin <maximopolnate@gmail.com>
2019-03-12 23:46:28 +0300

    test commit №2.3.6

---
+++ src/by/bnty/fitr/fusman/lab10/kek.java
@@ -0,0 +1,25 @@
+package by.bnty.fitr.fusman.lab10;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class kek {
+
+    public static void main(String[] args) {
+        try {
+            FileInputStream f = new FileInputStream("C:\\Users\\Admin\\Downloads\\lol.mp3");
+            try {
+                Player player = new Player(f);
+                player.play();
+            } catch (JavaLayerException e) {
+                e.printStackTrace();
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
